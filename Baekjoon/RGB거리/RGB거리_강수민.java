853a3c4880058ccf83ac1be0c75051a455b82c61
강수민 <kangsm423@gmail.com>
2024-09-03 22:34:40 +0900

    solved RGB거리 - 112ms 14.604mb

---
+++ Baekjoon/RGB거리/RGB거리_강수민.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N + 1][3];
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int R = Integer.parseInt(st.nextToken());
+            int G = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + R;
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + G;
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + B;
+        }
+
+        bw.write(Integer.toString(Math.min(dp[N][0], Math.min(dp[N][1], dp[N][2]))));
+        bw.flush();
+    }
+}
