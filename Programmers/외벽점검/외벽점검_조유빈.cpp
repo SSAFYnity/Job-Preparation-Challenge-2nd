ed429c77626a81994bfad6cd64fb4cf18d54ab2e
u0empty <yubeen00@naver.com>
2024-09-20 00:10:48 +0900

    solved 외벽점검 - 36.98ms 4.22mb

--- Programmers/외벽점검/외벽점검_조유빈.cpp
+++ Programmers/외벽점검/외벽점검_조유빈.cpp
@@ -4,80 +4,77 @@
 
 using namespace std;
 
-int Answer = 2e9;
-bool Select[8];
-vector<int> V;
+int answer = 2e9;
+bool selected[8];
+vector<int> v;
 
-int Min(int A, int B) {
-    if (A < B) return A;
-    return B;
+bool cmp(int a, int b) {
+    if (a > b) return true;
+    return false; 
 }
 
-bool Cmp(int A, int B) {
-    if (A > B) return true;
-    return false;
-}
+void check(int n, vector<int> weak, vector<int> dist) {
+    for (int i = 0; i < weak.size() - 1; i++) {
+        int idx = 0;
+        int cnt;
+        bool flag = false;
 
-void Check(vector<int> Weak, vector<int> Dist, int N) {
-    for (int i = 0; i < Weak.size() - 1; i++) {
-        int Weak_Index = 0;
-        int Cnt;
-        bool Flag = false;
-        
-        for (int j = 0; (j < V.size()) && (j + 1 < Answer) && (Flag == false); j++) {
-            int End = Weak[Weak_Index] + Dist[V[j]];
-            
-            while (End >= Weak[Weak_Index]) {
-                Weak_Index++;
-                
-                if (Weak_Index == Weak.size()) {
-                    Cnt = j + 1;
-                    Flag = true;
+        for (int j = 0; (j < v.size()) && (j + 1 < answer) && (flag == false); j++) {
+            int end = weak[idx] + dist[v[j]];
+            while (end >= weak[idx]) {
+                idx++;
+                if (idx == weak.size()) {
+                    cnt = j + 1;
+                    flag = true;
                     break;
                 }
             }
         }
-        
-        if (Flag == true) Answer = Min(Answer, Cnt);
-        
-        int Start_Value = Weak[0];
-        for (int j = 1; j < Weak.size(); j++) Weak[j - 1] = Weak[j];
-        Weak[Weak.size() - 1] = Start_Value + N;
+        if (flag == true) answer = min(answer, cnt);
+
+        int startValue = weak[0];
+        for (int j = 1; j < weak.size(); j++) {
+            weak[j - 1] = weak[j];
+        }
+        weak[weak.size() - 1] = startValue + n;
     }
 }
 
-void DFS(int Cnt, vector<int> Weak, vector<int> Dist, int N) {
-    if (Cnt == Dist.size()) {
-        Check(Weak, Dist, N);
+void dfs(int cnt, int n, vector<int> weak, vector<int> dist) {
+    if (cnt == dist.size()) {
+        check(n, weak, dist);
         return;
     }
-    
-    for (int i = 0; i < Dist.size(); i++) {
-        if (Select[i] == true) continue;
-        Select[i] = true;
-        V.push_back(i);
-        DFS(Cnt + 1, Weak, Dist, N);
-        V.pop_back();
-        Select[i] = false;
+
+    for (int i = 0; i < dist.size(); i++) {
+        if (selected[i] == true) continue;
+        selected[i] = true;
+        v.push_back(i);
+        dfs(cnt + 1, n, weak, dist);
+        v.pop_back();
+        selected[i] = false;
     }
 }
 
 int solution(int n, vector<int> weak, vector<int> dist) {
-    Answer = 2e9;
-    sort(dist.begin(), dist.end(), Cmp);
-    vector<int> Temp = weak;
-    
-    for (int i = 0; i < Temp.size() - 1; i++) {
-        int Start = Temp[0];
-        int End = Temp[Temp.size() - 1];
-        if (End - Start <= dist[0]) return 1;
-        
-        int Start_Value = Temp[0];
-        for (int j = 1; j < Temp.size(); j++) Temp[j - 1] = Temp[j];
-        Temp[Temp.size() - 1] = Start_Value + n;
+    answer = 2e9;
+    sort(dist.begin(), dist.end(), cmp);
+    vector<int> tmp = weak;
+
+    int l = tmp.size();
+    for (int i = 0; i < l - 1; i++) { // 왜 size - 1 일가
+        int start = tmp[0];
+        int end = tmp[l - 1];
+        if (end - start <= dist[0]) return 1;
+
+        int startValue = tmp[0];
+        for (int j = 1; j < l; j++) {
+            tmp[j - 1] = tmp[j];
+        }
+        tmp[l - 1] = startValue + n;
     }
-    
-    DFS(0, weak, dist, n);
-    if (Answer == 2e9) return -1;
-    return Answer;
+
+    dfs(0, n, weak, dist);
+    if (answer == 2e9) return -1;
+    return answer;
 }
